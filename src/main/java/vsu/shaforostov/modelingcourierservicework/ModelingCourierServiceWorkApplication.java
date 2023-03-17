eea65f2ce884ee9d1a7c8782dff27ca2b1e58e57
Misticksman <vadim02101@gmail.com>
2023-03-18 00:24:23 +0300

    initial project

---
+++ src/main/java/vsu/shaforostov/modelingcourierservicework/ModelingCourierServiceWorkApplication.java
@@ -0,0 +1,13 @@
+package vsu.shaforostov.modelingcourierservicework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModelingCourierServiceWorkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModelingCourierServiceWorkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/vsu/shaforostov/modelingcourierservicework/ModelingCourierServiceWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package vsu.shaforostov.modelingcourierservicework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModelingCourierServiceWorkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
